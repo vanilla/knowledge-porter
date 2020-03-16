0909478fc859388e5bd5edd9024dad5d128df182
Chris Chabilall <chris.c@vanillaforums.com>
2020-03-16 09:49:17 -0400

    Updated unit test

--- tests/ZendeskSourceTest.php
+++ tests/ZendeskSourceTest.php
@@ -13,10 +13,24 @@ use Vanilla\KnowledgePorter\Sources\ZendeskSource;
 
 class ZendeskSourceTest extends TestCase {
     public function testBasicReplaceUrls() {
-        $body = '<a href=\"https://raccoonworks.zendesk.com/hc/en-us/articles/360040814891\" target=\"_self\">Raccoon Works First Steps</a>';;
-        $expected = '<a href=\"https://dev.vanilla.localhost/kb/articles/aliases/zd-raccoon/hc/en-us/articles/360040814891" target=\"_self\">Raccoon Works First Steps</a>';
 
-        $actual = ZendeskSource::replaceUrls($body, '', '', '');
+        $body = '<p><strong>Justified Alignment</strong></p>\n<p class=\"wysiwyg-text-align-justify\">
+Optimism mass incarceration; academic philanthropy social impact correlation. Segmentation.
+</p>\n<p> </p>\n<span class=\"wysiwyg-font-size-large\"><strong>Links and hyperlinks</strong></span></p>\n
+<p>Google - <a href=\"https://www.google.com/\">https://www.google.com/</a></p>\n
+<p><a href=\"https://www.google.com/\" target=\"_self\">Search Google</a></p>\n<p>Link to another Zendesk article 
+<a href=\"https://raccoonworks.zendesk.com/hc/en-us/articles/360040814891" target=\"_self\">Raccoon Works First Steps</a></p>\n
+<p>Links within article: </p></p>';
+
+        $expected = '<p><strong>Justified Alignment</strong></p>\n<p class=\"wysiwyg-text-align-justify\">
+Optimism mass incarceration; academic philanthropy social impact correlation. Segmentation.
+</p>\n<p> </p>\n<span class=\"wysiwyg-font-size-large\"><strong>Links and hyperlinks</strong></span></p>\n
+<p>Google - <a href=\"https://www.google.com/\">https://www.google.com/</a></p>\n
+<p><a href=\"https://www.google.com/\" target=\"_self\">Search Google</a></p>\n<p>Link to another Zendesk article 
+<a href=\"https://dev.vanilla.localhost/kb/articles/aliases/zd-raccoon/hc/en-us/articles/360040814891" target=\"_self\">Raccoon Works First Steps</a></p>\n
+<p>Links within article: </p></p>';
+
+        $actual = ZendeskSource::replaceUrls($body, 'raccoonworks.zendesk.com', 'dev.vanilla.localhost', 'zd-raccoon');
         $this->assertSame($expected, $actual);
     }
 }
