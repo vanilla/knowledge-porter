612a1ba8fc13a63b94f60da0aed484a2b546531c
Eduardo GarciaJulia <eduardo.garciajulia@vanillaforums.com>
2020-04-16 18:13:34 -0400

    fix handling of exit code

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,24 +4,29 @@
  * @copyright 2009-2020 Vanilla Forums Inc.
  * @license Proprietary
  */
+try {
+    error_reporting(E_ALL | ~E_NOTICE | ~E_USER_NOTICE); //E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);
+    ini_set('display_errors', 'on');
+    ini_set('track_errors', 1);
 
-error_reporting(E_ALL | ~E_NOTICE | ~E_USER_NOTICE); //E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);
-ini_set('display_errors', 'on');
-ini_set('track_errors', 1);
+    date_default_timezone_set('UTC');
 
-date_default_timezone_set('UTC');
-
-$paths = [
-    __DIR__.'/../vendor/autoload.php', // locally
-    __DIR__.'/../../../autoload.php' // dependency
-];
-foreach ($paths as $path) {
-    if (file_exists($path)) {
-        require_once $path;
-        break;
+    $paths = [
+        __DIR__.'/../vendor/autoload.php', // locally
+        __DIR__.'/../../../autoload.php' // dependency
+    ];
+    foreach ($paths as $path) {
+        if (file_exists($path)) {
+            require_once $path;
+            break;
+        }
     }
+
+    $main = new \Vanilla\KnowledgePorter\Main();
+    $result = $main->run($argv);
+} catch (Throwable $t) {
+    $exitCode = $t->getCode() === 0 ? 1 : $t->getCode();
+    exit($exitCode);
 }
 
-$main = new \Vanilla\KnowledgePorter\Main();
-$result = $main->run($argv);
-return $result;
+exit ($result);
