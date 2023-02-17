35fb79d46f09d9c51375f026ea284f0581808a08
olivierlc <olamy-canuel@higherlogic.com>
2023-02-17 13:36:27 -0500

    set responses const

--- src/Sources/ZendeskMockSource.php
+++ src/Sources/ZendeskMockSource.php
@@ -11,9 +11,9 @@ use Exception;
 
 class ZendeskMockSource extends ZendeskSource
 {
-    const VALID_PAGE = 1;
-    const INVALID_PAGE = -1;
-    const EMPTY_PAGE = 0;
+    const VALID_RESPONSE = 1;
+    const INVALID_RESPONSE = -1;
+    const EMPTY_RESPONSE = 0;
 
     /**
      * Mock a call made to ZendeskSource::processKnowledgeBases().
@@ -59,12 +59,12 @@ class ZendeskMockSource extends ZendeskSource
     protected function returnMockRecords(int $mockValue): array
     {
         switch ($mockValue) {
-            case self::VALID_PAGE:
+            case self::VALID_RESPONSE:
                 return ["zd1", "zd2", "zd3"];
                 break;
-            case self::EMPTY_PAGE:
+            case self::EMPTY_RESPONSE:
                 return [];
-            case self::INVALID_PAGE:
+            case self::INVALID_RESPONSE:
             default:
                 throw new Exception("An error has occurred");
         }
